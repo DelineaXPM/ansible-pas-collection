12c9245cb6b5562b885a24def56d9545d1c54630
Sahil Wankhede <sahil.wankhede@c.delinea.com>
2024-09-10 07:59:36 -0400

    Update shell scripts for shebang issues

--- roles/delinea_vault/files/setup_ccli.sh
+++ roles/delinea_vault/files/setup_ccli.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Get CentrifyCLI package from Github 
 curl -1sLf 'https://github.com/centrify/centrifycli/releases/download/v1.0.5.0/ccli-v1.0.5.0-linux-x64.tar.gz' -o /tmp/ccli-1.0.5.0.tar.gz
@@ -8,10 +8,10 @@ rm /tmp/ccli-1.0.5.0.tar.gz
 # Make CentrifyCLI executable and create a symbolic link under /usr/bin
 chown root:root /opt/centrify/bin/ccli
 chmod +x /opt/centrify/bin/ccli
-ln -s /opt/centrify/bin/ccli /usr/bin/ccli
+ln -sf /opt/centrify/bin/ccli /usr/bin/ccli
 
 # Add environment variable to turn off globalisation for Dotnet Core
 export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
 
 # Save Tenant Url from parameter
-ccli --url $1 saveConfig
+ccli --url "$1" saveConfig
